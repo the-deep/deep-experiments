b4ce729dfeca0fad9d4150a41b53c3edef2dc228
Stefano Savare <stefano.savare@gmail.com>
2021-05-20 10:56:57 +0200

    Added constants
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- deep/constants.py
+++ deep/constants.py
@@ -4,4 +4,7 @@ ROOT_PATH = Path(__file__).parent.parent.absolute()
 DATA_PATH = ROOT_PATH / 'data'
 IMMAP_PATH = DATA_PATH / 'immap'
 
+SCRIPTS_PATH = ROOT_PATH / 'scripts'
+SCRIPTS_MODELS_PATH = SCRIPTS_PATH / 'models'
+
 SAGEMAKER_BUCKET = 'deep-experiments-sagemaker-bucket'
