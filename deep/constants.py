5ce47d0da75db94cf371744b42e78024561db33f
Stefano Savare <stefano.savare@gmail.com>
2021-05-20 17:07:48 +0200

    Changed name
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- deep/constants.py
+++ deep/constants.py
@@ -3,6 +3,7 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent.absolute()
 DATA_PATH = ROOT_PATH / "data"
 IMMAP_PATH = DATA_PATH / "immap"
+ALL_DATA_PATH = DATA_PATH / "frameworks_data"
 
 SCRIPTS_PATH = ROOT_PATH / "scripts"
 SCRIPTS_MODELS_PATH = SCRIPTS_PATH / "models"
