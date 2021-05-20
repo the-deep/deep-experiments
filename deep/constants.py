e0f619e139a2c3d5f4e4054bd9dd54dd774ba20a
Stefano Savare <stefano.savare@gmail.com>
2021-05-20 12:35:39 +0200

    Basic repo tools added
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- deep/constants.py
+++ deep/constants.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent.parent.absolute()
-DATA_PATH = ROOT_PATH / 'data'
-IMMAP_PATH = DATA_PATH / 'immap'
+DATA_PATH = ROOT_PATH / "data"
+IMMAP_PATH = DATA_PATH / "immap"
 
-SCRIPTS_PATH = ROOT_PATH / 'scripts'
-SCRIPTS_MODELS_PATH = SCRIPTS_PATH / 'models'
+SCRIPTS_PATH = ROOT_PATH / "scripts"
+SCRIPTS_MODELS_PATH = SCRIPTS_PATH / "models"
 
-SAGEMAKER_BUCKET = 'deep-experiments-sagemaker-bucket'
+SAGEMAKER_BUCKET = "deep-experiments-sagemaker-bucket"
--- scripts/models/stefano/train.py
+++ scripts/models/stefano/train.py
@@ -1,16 +1,20 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizerForSequenceClassification, Trainer, TrainingArguments
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizerForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from datasets import load_from_disk
 import pandas as pd
-import random
 import logging
 import sys
 import argparse
 import os
 import torch
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    
+
     # hyperparameters sent by the client are passed as command-line arguments to the script.
     parser.add_argument("--epochs", type=int, default=3)
     parser.add_argument("--train-batch-size", type=int, default=32)
@@ -18,7 +22,7 @@ if __name__ == "__main__":
     parser.add_argument("--warmup_steps", type=int, default=500)
     parser.add_argument("--model_name", type=str)
     parser.add_argument("--learning_rate", type=str, default=5e-5)
-    
+
     # Data, model, and output directories
     parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
     parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
@@ -26,7 +30,7 @@ if __name__ == "__main__":
     parser.add_argument("--training_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
     parser.add_argument("--test_dir", type=str, default=os.environ["SM_CHANNEL_TEST"])
     args, _ = parser.parse_known_args()
-    
+
     # Set up logging
     logger = logging.getLogger(__name__)
     logging.basicConfig(
@@ -34,14 +38,14 @@ if __name__ == "__main__":
         handlers=[logging.StreamHandler(sys.stdout)],
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
-    
+
     # load datasets
     # train_dataset = load_from_disk(args.training_dir)
     train_df = pd.read_pickle(args.training_dir)
-    test_df = None # load_from_disk(args.test_dir)
-    logger.info(f" loaded train_dataset length is: {len(train_dataset)}")
-    logger.info(f" loaded test_dataset length is: {len(test_dataset)}")
-    
+    test_df = None  # load_from_disk(args.test_dir)
+    logger.info(f" loaded train_dataset length is: {train_df.shape}")
+    logger.info(f" loaded test_dataset length is: {test_df.shape}")
+
     # compute metrics function for binary classification
     def compute_metrics(pred):
         labels = pred.label_ids
@@ -55,7 +59,10 @@ if __name__ == "__main__":
     model = AutoModelForSequenceClassification.from_pretrained(args.model_name)
 
     train_encodings = tokenizer(list(train_df.sentence_text), truncation=True, padding=True)
+    test_encodings = tokenizer(list(test_df.sentence_text), truncation=True, padding=True)
+
     train_labels = list(train_df.sector_ids)
+    test_labels = list(test_df.sector_ids)
 
     class Dataset(torch.utils.data.Dataset):
         def __init__(self, encodings, labels):
@@ -64,15 +71,15 @@ if __name__ == "__main__":
 
         def __getitem__(self, idx):
             item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-            item['labels'] = torch.tensor(self.labels[idx])
+            item["labels"] = torch.tensor(self.labels[idx])
             return item
 
         def __len__(self):
             return len(self.labels)
 
-
     train_dataset = Dataset(train_encodings, train_labels)
-    
+    test_dataset = Dataset(test_encodings, test_labels)
+
     # define training args
     training_args = TrainingArguments(
         output_dir=args.model_dir,
@@ -84,7 +91,7 @@ if __name__ == "__main__":
         logging_dir=f"{args.output_data_dir}/logs",
         learning_rate=float(args.learning_rate),
     )
-    
+
     # create Trainer instance
     trainer = Trainer(
         model=model,
@@ -93,20 +100,18 @@ if __name__ == "__main__":
         train_dataset=train_dataset,
         eval_dataset=test_dataset,
     )
-    
+
     # train model
     trainer.train()
-    
+
     # evaluate model
     eval_result = trainer.evaluate(eval_dataset=test_dataset)
-    
+
     # writes eval result to file which can be accessed later in s3 ouput
     with open(os.path.join(args.output_data_dir, "eval_results.txt"), "w") as writer:
         print(f"***** Eval results *****")
         for key, value in sorted(eval_result.items()):
             writer.write(f"{key} = {value}\n")
-    
+
     # Saves the model to s3
     trainer.save_model(args.model_dir)
-    
-    
