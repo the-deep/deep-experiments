f52b421ce940e46353c64efaf04b8d3a4e8afccd
Stefano Savare <stefano.savare@gmail.com>
2021-05-20 17:32:38 +0200

    Added classes
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- deep/constants.py
+++ deep/constants.py
@@ -9,3 +9,30 @@ SCRIPTS_PATH = ROOT_PATH / "scripts"
 SCRIPTS_MODELS_PATH = SCRIPTS_PATH / "models"
 
 SAGEMAKER_BUCKET = "deep-experiments-sagemaker-bucket"
+
+DIMENSION_CLASSES = [
+    "Shock Informaton",
+    "Effects Systems And Networks",
+    "Effects On Population",
+    "Capacities & Response",
+    "At Risk",
+    "Scope & Scale",
+    "Impact",
+    "Humanitarian Conditions",
+    "Priorities",
+    "Context",
+]
+
+SECTOR_CLASSES = [
+    "Agricolture",
+    "Cross",
+    "Education",
+    "Food Security",
+    "Health",
+    "Livelihoods",
+    "Logistics",
+    "Nutrition",
+    "Protection",
+    "Shelter",
+    "Wash",
+]
