cd4f8ae4a2135b908ecf4ea1fc1b8ed75a117c67
Stefano Savare <stefano.savare@gmail.com>
2021-05-27 10:38:38 +0200

    Added predictions on test set, training output, working metrics
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- scripts/training/stefano/pytorch_estimator_base/train.py
+++ scripts/training/stefano/pytorch_estimator_base/train.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import torch
 from pathlib import Path
+import pickle
 
 from transformers import (
     DistilBertForSequenceClassification,
@@ -110,12 +111,15 @@ if __name__ == "__main__":
 
     # evaluate model
     eval_result = trainer.evaluate(eval_dataset=test_dataset)
+    predictions = trainer.predict(test_dataset)
 
     # writes eval result to file which can be accessed later in s3 ouput
     with open(os.path.join(args.output_data_dir, "eval_results.txt"), "w") as writer:
         print(f"***** Eval results *****")
         for key, value in sorted(eval_result.items()):
             writer.write(f"{key} = {value}\n")
+    with open(Path(args.output_data_dir) / "test_predictions.pickle", "wb") as f:
+        pickle.dump(predictions, f)
 
     # Saves the model to s3
     trainer.save_model(args.model_dir)
