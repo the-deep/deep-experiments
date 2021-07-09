b33c598d27b0ce043e68e4703cce61a94e633b90
tnagorra <weathermist@gmail.com>
2021-07-09 14:42:59 +0545

    Create separate Row component to store feedback state

--- scripts/testing/offline_testing/template/frontend/src/CustomTableComponent.tsx
+++ scripts/testing/offline_testing/template/frontend/src/CustomTableComponent.tsx
@@ -3,70 +3,115 @@ import {
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
-import React, { ReactNode } from "react"
+import React, { useState, useCallback } from "react"
+
 import './style.css';
 
+interface Datum {
+  Entry: string;
+  'Tagger Name': string;
+  Pillars: string;
+  Sectors: string;
+  'Matched Sentences': string;
+  'Predicted Pillars': string;
+  'Predicted Sectors': string;
+}
+
+interface RowProps {
+  item: Datum;
+  onSubmit: (value: { feedback: string, entry: string }) => void;
+}
+function Row(props: RowProps) {
+  const {
+    item,
+    onSubmit,
+  } = props;
+
+  const [feedback, setFeedback] = useState('');
+  const handleFeedbackChange = useCallback(
+    (event: any) => {
+      setFeedback(event.target.value);
+    },
+    [],
+  );
+
+  const handleSubmit = useCallback(
+    () => {
+      onSubmit({ entry: item['Entry'], feedback });
+    },
+    [onSubmit, item, feedback],
+  );
+
+  return (
+    <React.Fragment>
+      <tr>
+        <td width="30%">{item['Entry']}</td>
+        <td>{item['Tagger Name']}</td>
+        <td>{item['Pillars']}</td>
+        <td>{item['Sectors']}</td>
+        <td width="30%">{item['Matched Sentences']}</td>
+        <td>{item['Predicted Pillars']}</td>
+        <td>{item['Predicted Sectors']}</td>
+      </tr>
+      <tr>
+        <td colSpan={7}>
+          <form
+            onSubmit={handleSubmit}
+          >
+            <textarea
+              value={feedback}
+              name="feedback"
+              onChange={handleFeedbackChange}
+            />
+            <button>
+              Submit Feedback
+            </button>
+          </form>
+        </td>
+      </tr>
+    </React.Fragment>
+  );
+}
+
 interface State {
-  feedback: string,
-  entry: string
+  data: Datum[];
 }
 
 class CustomTableComponent extends StreamlitComponentBase<State> {
-
   constructor(props: any) {
     super(props);
-    this.state = {entry: '', feedback: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    const data: Datum[] = JSON.parse(this.props.args["data"]);
+    this.state = {
+      data,
+    };
   }
 
-  handleChange = (event: any): void => {
-   this.setState({feedback: event.target.value});
-  }
-
-  handleSubmit = (entry: any): void => {
-    let data: any = {"feedback": this.state.feedback, "entry": entry}
-    this.setState(
-      () => Streamlit.setComponentValue(data)
-    )
+  handleSubmit = (value: { feedback: string, entry: string }) => {
+    Streamlit.setComponentValue(value);
   }
 
   render() {
-    var data = JSON.parse(this.props.args["data"])
-    const listItems = data.map((item: any, key: number) =>
-        <React.Fragment key={"item-" + key}>
-          <tr key={key}>
-            <td width="30%">{item['Entry']}</td>
-            <td>{item['Tagger Name']}</td>
-            <td>{item['Pillars']}</td>
-            <td>{item['Sectors']}</td>
-            <td width="30%">{item['Matched Sentences']}</td>
-            <td>{item['Predicted Pillars']}</td>
-            <td>{item['Predicted Sectors']}</td>
-          </tr>
-          <tr key={key}>
-            <td colSpan={7}>
-                <form onSubmit={()=>this.handleSubmit(item["Entry"])} id={"form-" + key}>
-                  <textarea id={"feedback-" + key}  value={this.state.feedback} name="feedback" onChange={this.handleChange}/>
-                  <button>Submit Feedback</button>
-                </form>
-            </td>
-          </tr>
-        </React.Fragment>
-    );
+    const { data } = this.state;
+
     return (
-          <table>
-            <tr>
-              <th>Entry</th>
-              <th>Tagger Name</th>
-              <th>Pillars</th>
-              <th>Sectors</th>
-              <th>Matched Sentences</th>
-              <th>Predicted Pillars</th>
-              <th>Predicted Sectors</th>
-            </tr>
-            {listItems}
-          </table>
+      <table>
+        <tr>
+          <th>Entry</th>
+          <th>Tagger Name</th>
+          <th>Pillars</th>
+          <th>Sectors</th>
+          <th>Matched Sentences</th>
+          <th>Predicted Pillars</th>
+          <th>Predicted Sectors</th>
+        </tr>
+        {data.map((datum) => (
+          <Row
+            key={datum.Entry}
+            item={datum}
+            onSubmit={this.handleSubmit}
+          />
+        ))}
+      </table>
     );
   }
 }
