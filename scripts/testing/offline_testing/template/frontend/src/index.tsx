a4c90731d249629aede6609c19f9dd57851ef3f9
bimal prasad pandey <pandeybi125@gmail.com>
2021-07-09 14:22:45 +0545

    Add table components

---
+++ scripts/testing/offline_testing/template/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import CustomTableComponent from "./CustomTableComponent"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CustomTableComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
