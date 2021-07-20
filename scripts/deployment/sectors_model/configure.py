e1bc33d2fce290db4d8d1c52ecb21dd3583358e9
Stefano Savare <stefano.savare@gmail.com>
2021-07-20 19:09:59 +0200

    Updated CI
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- scripts/deployment/sectors_model/configure.py
+++ scripts/deployment/sectors_model/configure.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-from cloudpathlib import CloudPath
 import yaml
 
+# import os
+
+from cloudpathlib import CloudPath
 import boto3
 
 current_path = Path(__file__).parent
@@ -13,8 +15,12 @@ if __name__ == "__main__":
 
     model = CloudPath(config["model"])
 
+    # command = f'aws s3 cp '
+    # os.system(f'')
     s3 = boto3.client("s3")
     bucket_name = model.cloud_prefix + model.bucket
     s3.download_file(
-        str(model.bucket), str(model).replace(bucket_name, ""), str(current_path / "conda.yaml")
+        str(model.bucket),
+        str(model).replace(bucket_name, "") + "/conda.yaml",
+        str(current_path / "conda.yaml"),
     )
