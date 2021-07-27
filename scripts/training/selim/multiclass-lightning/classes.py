b272332754486d8c655cc7e65903baeb97a2a5f5
sfekih <selim.fekih@epfl.ch>
2021-07-27 10:47:47 +0200

    sagemaker training jobs

---
+++ scripts/training/selim/multiclass-lightning/classes.py
@@ -0,0 +1,463 @@
+
+from typing import Optional
+
+from tqdm.auto import tqdm
+
+import torchmetrics
+from torchmetrics.functional import auroc
+
+import pytorch_lightning as pl
+from pytorch_lightning.core.decorators import auto_move_data
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+
+from transformers import (
+    AdamW,
+    AutoModel,
+)
+from transformers.optimization import (
+    get_linear_schedule_with_warmup,
+)
+
+class CustomDataset(Dataset):
+    def __init__(self, dataframe, tagname_to_tagid, tokenizer, max_len:int=128):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+
+        self.excerpt_text = dataframe["excerpt"].tolist(
+        ) if dataframe is not None else None
+
+        self.targets = self.data['target'].tolist(
+        ) if dataframe is not None else None
+
+        self.entry_ids = self.data['entry_id'].tolist(
+        ) if dataframe is not None else None
+
+        self.tagname_to_tagid = tagname_to_tagid
+        self.tagid_to_tagname = list(tagname_to_tagid.keys())
+        self.max_len = max_len
+
+    def encode_example(self,
+                       excerpt_text: str,
+                       index=None,
+                       as_batch: bool = False):
+        
+        inputs = self.tokenizer(excerpt_text,
+                                None,
+                                truncation=True,
+                                add_special_tokens=True,
+                                max_length=self.max_len,
+                                padding="max_length",
+                                return_token_type_ids=True)
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+        
+        targets = None
+        if self.targets:
+            target_indices = [
+                self.tagname_to_tagid[target]
+                for target in self.targets[index]
+                if target in self.tagname_to_tagid
+            ]
+            targets = np.zeros(len(self.tagname_to_tagid), dtype=np.int)
+            targets[target_indices] = 1
+        encoded = {
+            'ids':
+            torch.tensor(ids, dtype=torch.long),
+            'mask':
+            torch.tensor(mask, dtype=torch.long),
+            'token_type_ids':
+            torch.tensor(token_type_ids, dtype=torch.long),
+            'targets':
+            torch.tensor(targets, dtype=torch.float32)
+            if targets is not None else None,
+            'entry_id':
+            self.entry_ids[index]
+        }
+
+        if as_batch:
+            return {
+                "ids": encoded["ids"].unsqueeze(0),
+                "mask": encoded["mask"].unsqueeze(0),
+                "token_type_ids": encoded["ids"].unsqueeze(0)
+            }
+        return encoded
+
+    def __len__(self):
+        return len(self.excerpt_text)
+
+    def __getitem__(self, index):
+        excerpt_text = str(self.excerpt_text[index])
+        return self.encode_example(excerpt_text, index)
+
+
+class Model(nn.Module):
+    def __init__(self, model_name_or_path: str, num_labels:int, dropout_rate=0.3, output_length=384):
+        super().__init__()
+        self.l1 = AutoModel.from_pretrained(model_name_or_path)
+        #batch normlisation to be integrated
+        #self.l2 = torch.nn.BatchNorm1d(output_length)
+        self.l2 = torch.nn.Dropout(dropout_rate)
+        self.l3 = torch.nn.Linear(output_length, num_labels)
+        
+    def forward(self, inputs):
+        output = self.l1(inputs["ids"],
+                        attention_mask=inputs["mask"],)
+        output = output.last_hidden_state
+        output = self.l2(output)
+        output = self.l3(output)
+        return output[:, 0, :]
+
+
+class Transformer(pl.LightningModule):
+    def __init__(self,
+                 model_name_or_path: str,
+                 tagname_to_tagid,
+                 empty_dataset: CustomDataset,
+                 train_dataset,
+                 val_dataset,
+                 train_params,
+                 val_params,      
+                 weight_classes,
+                 tokenizer,  
+                 pred_threshold: float = .5,
+                 learning_rate: float = 1e-5,
+                 adam_epsilon: float = 1e-8,
+                 warmup_steps: int = 500,
+                 weight_decay: float = 0.1,
+                 train_batch_size: int = 32,
+                 eval_batch_size: int = 32,
+                 eval_splits: Optional[list] = None,
+                 dropout_rate: float = 0.3,
+                 max_len:int = 128,
+                 output_length=384,
+
+                 **kwargs):
+
+
+        super().__init__()
+        self.output_length = output_length
+        self.save_hyperparameters()
+        self.tagname_to_tagid = tagname_to_tagid
+        self.num_labels = len(tagname_to_tagid)
+        self.max_len = max_len
+        self.model = Model(model_name_or_path, self.num_labels, dropout_rate, self.output_length)
+        self.tokenizer = tokenizer
+        self.val_params = val_params
+
+
+        if any(weight_classes):
+            self.use_weights = True
+            self.weight_classes = torch.tensor(weight_classes).to('cuda')
+        else:
+            self.use_weights = False
+        self.empty_dataset = empty_dataset
+        self.pred_threshold = pred_threshold
+        self.training_loader = self.get_loaders (train_dataset,
+                                    train_params,
+                                    tagname_to_tagid,
+                                    self.tokenizer,
+                                    max_len
+                                    )
+        self.val_loader = self.get_loaders (val_dataset,
+                                    val_params,
+                                    tagname_to_tagid,
+                                    self.tokenizer,
+                                    max_len
+                                    )
+        self.f1_score_train = torchmetrics.F1(
+            num_classes=2,
+            threshold=self.pred_threshold,
+            average='macro',
+            mdmc_average="samplewise",
+            ignore_index=None,
+            top_k=None,
+            multiclass=True,
+            compute_on_step=True,
+            dist_sync_on_step=False,
+            process_group=None,
+            dist_sync_fn=None,
+        )
+
+        self.f1_score_val = torchmetrics.F1(
+            num_classes=2,
+            threshold=self.pred_threshold,
+            average='macro',
+            mdmc_average="samplewise",
+            ignore_index=None,
+            top_k=None,
+            multiclass=True,
+            compute_on_step=True,
+            dist_sync_on_step=False,
+            process_group=None,
+            dist_sync_fn=None,
+        )
+        
+    @auto_move_data
+    def forward(self, inputs):
+        output = self.model(inputs)
+        return output
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(batch)
+        if self.use_weights:
+            loss = F.binary_cross_entropy_with_logits(outputs,
+                                                    batch["targets"],
+                                                    weight=self.weight_classes)
+        else:
+            loss = F.binary_cross_entropy_with_logits(outputs,
+                                                    batch["targets"])
+
+        self.f1_score_train(torch.sigmoid(outputs),
+                            batch["targets"].to(dtype=torch.long))
+        self.log("train_f1", self.f1_score_train, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        outputs = self(batch)
+        val_loss = F.binary_cross_entropy_with_logits(outputs,
+                                                      batch["targets"])
+
+        self.f1_score_val(torch.sigmoid(outputs),
+                          batch["targets"].to(dtype=torch.long))
+        self.log("val_f1",
+                 self.f1_score_val,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=False)
+        
+        self.log("val_loss",
+                 val_loss,
+                 on_step=True,
+                 on_epoch=True,
+                 prog_bar=True,
+                 logger=False)
+        return {'val_loss': val_loss, 'val_f1': self.f1_score_val}
+
+    def test_step(self, batch, batch_nb):
+        logits = self(batch)
+        preds = (torch.sigmoid(logits) > .5)
+        return {"preds": preds, "targets_i": batch["targets"]}
+
+    def on_test_epoch_end(self, outputs):
+        preds = torch.cat([output["preds"] for output in outputs]).cpu()
+        targets = torch.cat([output["targets_i"] for output in outputs]).cpu()
+
+        for i in range(targets.shape[1]):
+            class_roc_auc = auroc(preds[:, i], targets[:, i])
+            self.log(
+                f"{self.empty_dataset.sectorid_to_sectorname[i]}_roc_auc/Train",
+                class_roc_auc)
+            class_f1 = metrics.f1_score(targets[:, i], preds[:, i])
+            self.log(
+                f"{self.empty_dataset.sectorid_to_sectorname[i]}_f1/Train",
+                class_f1)
+
+    def get_loaders (self, dataset, params, tagname_to_tagid,  tokenizer, max_len:int=128):
+                
+        set = CustomDataset(dataset, tagname_to_tagid, tokenizer, max_len)
+        
+        loader = DataLoader(set, **params)
+        return loader
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=None):
+        output = self(batch)
+        return {"logits": output}
+
+    def on_predict_epoch_end(self, outputs):
+        logits = torch.cat([output["logits"] for output in outputs[0]])
+        preds = torch.sigmoid(logits) >= self.pred_threshold
+        pred_classes = []
+        for pred in preds:
+            pred_classes_i = [
+                self.empty_dataset.sectorid_to_sectorname[i]
+                for i, p in enumerate(pred) if p
+            ]
+            pred_classes.append(pred_classes_i)
+        self.log({"pred_classes": pred_classes})
+
+    def custom_predict(self, validation_dataset, return_all=False):
+        validation_loader = self.get_loaders (validation_dataset,
+                                                self.val_params,
+                                                self.tagname_to_tagid,
+                                                self.tokenizer,
+                                                self.max_len
+                                                )
+        if self.device.type == "cpu":
+            self.to("cuda")
+        self.eval()
+        self.freeze()
+        indexes = []
+        y_true = []
+        logit_predictions = []
+
+
+        with torch.no_grad():
+            for batch in tqdm(validation_loader, total=len(validation_loader.dataset)//validation_loader.batch_size):
+                
+                logits = self({"ids": batch["ids"].to('cuda'),
+                                "mask": batch["mask"].to('cuda'),
+                                "token_type_ids": batch["token_type_ids"].to('cuda')})
+
+                y_true.append(batch["targets"].numpy().astype(np.int))
+                indexes.append(batch["entry_id"].numpy().astype(np.int))
+
+                logits_to_array = np.array([np.array(t) for t in logits.cpu()])
+                logit_predictions.append(logits_to_array)
+                
+        y_true = np.concatenate(y_true)
+        indexes = np.concatenate(indexes)
+        logit_predictions = np.concatenate(logit_predictions)
+
+        def sigmoid (x):
+            return 1 / (1+np.exp(-x))
+
+        logit_predictions = sigmoid(logit_predictions)
+        predictions = []
+        y_true_final = []
+        indexes_final = []
+        #postprocess predictions
+        for i in range (logit_predictions.shape[0]):
+            row_pred = np.array([0] * self.num_labels)
+            row_logits = logit_predictions[i,:]
+            if np.all(row_logits<self.pred_threshold):
+                row_pred[np.argmax(row_logits)] = 1
+            else :
+                row_pred[row_logits>self.pred_threshold] = 1
+                predictions.append(row_pred)
+                y_true_final.append(y_true[i])
+                indexes_final.append(indexes[i])
+
+        if return_all:
+            return np.array(predictions), y_true_final, indexes_final, zip(indexes, logit_predictions)
+
+        return np.array(predictions), np.array(y_true_final)
+
+    def total_steps(self) -> int:
+        """The number of total training steps that will be run. Used for lr scheduler purposes."""
+        self.dataset_size = len(self.train_dataloader().dataset)
+        num_devices = max(1, self.hparams.gpus)  # TODO: consider num_tpu_cores
+        effective_batch_size = self.hparams.train_batch_size * self.hparams.accumulate_grad_batches * num_devices
+        return (self.dataset_size /
+                effective_batch_size) * self.hparams.max_epochs
+
+    def configure_optimizers(self):
+        "Prepare optimizer and schedule (linear warmup and decay)"
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [
+                    p for n, p in model.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay":
+                self.hparams.weight_decay,
+            },
+            {
+                "params": [
+                    p for n, p in model.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay":
+                0.0,
+            },
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters,
+                          lr=self.hparams.learning_rate,
+                          eps=self.hparams.adam_epsilon)
+
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.hparams.warmup_steps,
+            num_training_steps=self.total_steps())
+        scheduler = {
+            'scheduler': scheduler,
+            'interval': 'step',
+            'frequency': 1
+        }
+        return [optimizer], [scheduler]
+
+    def train_dataloader(self):
+        return self.training_loader
+
+    def val_dataloader(self):
+        return self.val_loader
+
+    def compute_metrics (self, preds_val_all, y_true, tagname_to_tagid):
+        f1_scores = []
+        recalls = []
+        precisions = []
+        accuracies = []
+         
+        for tag_name, tag_id in tagname_to_tagid.items():
+            cls_rprt = metrics.classification_report(y_true[:, tag_id], preds_val_all[:, tag_id], output_dict=True)
+            precisions.append(cls_rprt["macro avg"]["precision"])
+            recalls.append(cls_rprt["macro avg"]["recall"])
+            f1_scores.append(cls_rprt["macro avg"]["f1-score"])
+            accuracies.append(cls_rprt["accuracy"])
+
+        metrics_df = pd.DataFrame({
+            "Sector": list(tagname_to_tagid.keys()),
+            "Precision": precisions,
+            "Recall": recalls,
+            "F1 Score": f1_scores,
+            "Accuracy": accuracies,
+        })
+        metrics_df.loc['mean'] = metrics_df.mean()
+        return metrics_df
+
+    def get_results_pillar_from_subpillar (self, preds_val_all, y_true):
+        list_tags_subpillars = list(self.tagname_to_tagid.keys())
+        list_tags_pillars = sorted(list(set([tags.split('->')[0] for tags in list_tags_subpillars])))
+        pillars_tagname_to_tagid = {tag:i for i, tag in enumerate(list(sorted(list_tags_pillars)))}
+
+        n_subpillars_per_pillar = [len(list(filter(lambda x: pillar in x, list_tags_subpillars))) for pillar in list_tags_pillars]
+        
+        def subpillars_to_pillars (y):
+            print(y.shape)
+            pillars_true_y = []
+            for row_nb in range (y.shape[0]):
+                row = y[row_nb, :]
+                nb_pillars = len(n_subpillars_per_pillar)
+                result = np.array([0] * nb_pillars)
+                count=0
+                for pillar_nb in range (nb_pillars):
+
+                    supillar_nb_tmp = n_subpillars_per_pillar[pillar_nb]
+
+                    if np.any(row[count:count+supillar_nb_tmp]):
+                        result[pillar_nb] = 1
+
+                    count += supillar_nb_tmp
+
+                if np.any(row[count:]):
+                    result[-1] = 1
+
+                pillars_true_y.append(result)
+            
+            return np.array(pillars_true_y)
+
+        y_true_pillars = subpillars_to_pillars (y_true)
+        preds_val_pillars = subpillars_to_pillars (preds_val_all)
+
+        return self.compute_metrics (preds_val_pillars, y_true_pillars, pillars_tagname_to_tagid)
+    
+    def custom_eval(self, 
+                    validation_dataset):
+
+        preds_val_all, y_true = self.custom_predict(validation_dataset)
+        print('ratio of evaluated sentences (%):', len(y_true) * 100 / validation_dataset.shape[0])
+        metrics_subpillars = self.compute_metrics(preds_val_all, y_true, self.tagname_to_tagid)
+        metrics_pillars = self.get_results_pillar_from_subpillar (preds_val_all, y_true)
+        
+        return metrics_pillars, metrics_subpillars
---
+++ scripts/training/selim/multiclass-lightning/train.py
@@ -0,0 +1,153 @@
+import sys
+import os
+import argparse
+import logging
+
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
+import timeit
+
+from utils import (
+    read_merge_data,
+    preprocess_data,
+    tagname_to_id,
+    compute_weights,
+
+)
+from generate_models import train_on_specific_targets
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument("--epochs", type=int, default=3)
+    parser.add_argument("--train_batch_size", type=int, default=32)
+    parser.add_argument("--val_batch_size", type=int, default=64)
+
+    parser.add_argument("--max_len", type=int, default=128)
+    parser.add_argument("--warmup_steps", type=int, default=50)
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument("--learning_rate", type=float, default=3e-5)
+    parser.add_argument("--dropout_rate", type=float, default=0.3)
+    parser.add_argument("--pred_threshold", type=float, default=0.4)
+    parser.add_argument("--output_length", type=int, default=384)
+
+    parser.add_argument("--model_name", type=str, default='microsoft/xtremedistil-l6-h384-uncased')
+    parser.add_argument("--tokenizer_name", type=str, default='microsoft/xtremedistil-l6-h384-uncased')
+    #parser.add_argument("--log_every_n_steps", type=int, default=10)
+    #parser.add_argument("--n_classes", type=int, default=6)
+    parser.add_argument("--method_language", type=str, default='keep all')
+
+    # Data, model, and output directories
+    parser.add_argument("--output-data-dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
+    parser.add_argument("--training_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+    parser.add_argument("--val_dir", type=str, default=os.environ["SM_CHANNEL_TEST"])
+    args, _ = parser.parse_known_args()
+
+
+    # Set up logging
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.getLevelName("INFO"),
+        handlers=[logging.StreamHandler(sys.stdout)],
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    # load datasets
+
+    ########################################
+
+    
+
+    print('importing data ............')
+    all_dataset = read_merge_data (args.training_dir, args.val_dir, data_format='pickle')
+
+    train_params = {
+        'batch_size': args.train_batch_size,
+        'shuffle': True,
+        'num_workers': 4
+    }
+
+    val_params = {
+        'batch_size': args.val_batch_size,
+        'shuffle': False,
+        'num_workers': 4
+        }
+
+    train_df, val_df = preprocess_data(all_dataset, 
+                                   perform_augmentation=False,
+                                   method=args.method_language)
+    
+    tags_ids = tagname_to_id (all_dataset.target)
+    list_tags = list(tags_ids.keys())
+
+    number_data_classes = []
+    for tag in list_tags:
+        nb_data_in_class = train_df.target.apply(lambda x: tag in (x)).sum()
+        number_data_classes.append(nb_data_in_class)
+
+    weights = compute_weights (number_data_classes, train_df.shape[0])
+
+    weights = [weight if weight<5 else weight**2 for weight in weights ]
+
+
+    log_dir_name = "-".join(args.model_name.split("/"))
+    PATH_NAME = args.model_dir
+    if not os.path.exists(PATH_NAME):
+        os.makedirs(PATH_NAME)
+
+    early_stopping_callback = EarlyStopping(monitor='val_f1',
+                                        patience=2,
+                                       mode='max')
+
+    checkpoint_callback_params = {
+        'save_top_k': 1,
+        'verbose': True,
+        'monitor': "val_f1",
+        'mode': "max"
+    }
+    dirpath_pillars = str(f"{args.model_dir}/checkpoints-subpillars-{log_dir_name}")
+    checkpoint_callback_pillars = ModelCheckpoint(
+    dirpath=dirpath_pillars,
+    **checkpoint_callback_params
+    )
+
+    
+    model_subpillars = train_on_specific_targets(train_df,
+                                                val_df,
+                                                f"subpillars-{log_dir_name}-",
+                                                dirpath_pillars,
+                                                args.model_name,
+                                                args.tokenizer_name,
+                                                early_stopping_callback,
+                                                checkpoint_callback_pillars,
+                                                gpu_nb=1,
+                                                train_params=train_params,
+                                                val_params=val_params,
+                                                MAX_EPOCHS=args.epochs,
+                                                dropout_rate=args.dropout_rate,
+                                                weight_classes=weights,
+                                                weight_decay=args.weight_decay,
+                                                learning_rate=args.learning_rate,
+                                                max_len=args.max_len,
+                                                warmup_steps=args.warmup_steps,
+                                                pred_threshold=float(args.pred_threshold),
+                                                output_length=args.output_length)
+
+
+    start = timeit.default_timer()
+
+    metrics_pillars, metrics_subpillars = model_subpillars.custom_eval(val_df)
+
+    stop = timeit.default_timer()
+
+    print('Time to predict 100 sentences: ', 100*(stop - start) / val_df.shape[0])  
+    print('subpillars: ', metrics_subpillars.loc['mean'])
+    print('pillars: ', metrics_pillars.loc['mean'])
+
+    metrics_subpillars.to_csv(f"{args.output_data_dir}/results_subpillars.csv")
+    metrics_pillars.to_csv(f"{args.output_data_dir}/results_pillars.csv")
+
+    
