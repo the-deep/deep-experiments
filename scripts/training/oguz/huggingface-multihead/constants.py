3f5891ae30801bbf05977d9f6d5308218dc55e66
Oğuz Kaan Yüksel <okyksl@gmail.com>
2021-08-02 11:37:44 +0200

    chore(constants): add 2D matrix tags

--- scripts/training/oguz/huggingface-multihead/constants.py
+++ scripts/training/oguz/huggingface-multihead/constants.py
@@ -45,3 +45,61 @@ SUBPILLARS_1D = [
         "Information->Information Challenges",
     ],
 ]
+
+SECTORS = [
+    "Agriculture",
+    "Cross",
+    "Education",
+    "Food Security",
+    "Health",
+    "Livelihoods",
+    "Logistics",
+    "Nutrition",
+    "Protection",
+    "Shelter",
+    "WASH",
+]
+
+PILLARS_2D = [
+    "Humanitarian Conditions",
+    "Capacities & Response",
+    "Impact",
+    "Priority Interventions",
+    "People At Risk",
+    "Priority Needs",
+]
+
+SUBPILLARS_2D = [
+    [
+        "Humanitarian Conditions->Coping Mechanisms",
+        "Humanitarian Conditions->Living Standards",
+        "Humanitarian Conditions->Number Of People In Need",
+        "Humanitarian Conditions->Physical And Mental Well Being",
+    ],
+    [
+        "Capacities & Response->International Response",
+        "Capacities & Response->National Response",
+        "Capacities & Response->Number Of People Reached",
+        "Capacities & Response->Response Gaps",
+    ],
+    [
+        "Impact->Driver/Aggravating Factors",
+        "Impact->Impact On People",
+        "Impact->Impact On People Or Impact On Services",
+        "Impact->Impact On Services",
+        "Impact->Impact On Systems And Services",
+        "Impact->Number Of People Affected",
+    ],
+    [
+        "Priority Interventions->Expressed By Humanitarian Staff",
+        "Priority Interventions->Expressed By Population",
+    ],
+    [
+        "People At Risk->Number Of People At Risk",
+        "People At Risk->Risk And Vulnerabilities",
+    ],
+    [
+        "Priority Needs->Expressed By Humanitarian Staff",
+        "Priority Needs->Expressed By Population",
+    ],
+]
