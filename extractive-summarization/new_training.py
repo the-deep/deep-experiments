bd975fa15d0c32c0ffa4900215498ee47819c704
Bibek Pandey <bewakepandey@gmail.com>
2022-09-12 11:36:03 +0545

    Log ml params

--- extractive-summarization/new_training.py
+++ extractive-summarization/new_training.py
@@ -1,8 +1,11 @@
 import tqdm
 import torch
 import time
+from copy import copy
 
-from rogue import Rouge
+import mlflow
+
+from rouge import Rouge
 from transformers import AutoTokenizer
 
 from train import get_test_train_data, get_args, LABEL_NAMES, get_separate_layer_groups
@@ -11,6 +14,7 @@ from model import Model
 
 def train_model(model, train, test, loss_fn, output_dim, lr=0.001, batch_size=512, n_epochs=10):
     param_lrs = [{"params": param, "lr": lr} for param in model.parameters()]
+    print("MODEL PARAMS", model.parameters())
     optimizer = torch.optim.Adam(param_lrs, lr=lr)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda epoch: 0.6**epoch)
     train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
@@ -20,6 +24,7 @@ def train_model(model, train, test, loss_fn, output_dim, lr=0.001, batch_size=51
     avg_val_loss = 0
     best_loss = float("inf")
     # checkpoint_weights = [2 ** epoch for epoch in range(n_epochs)]
+    best_state_dict = None
     for epoch in range(n_epochs):
         start_time = time.time()
 
@@ -53,14 +58,18 @@ def train_model(model, train, test, loss_fn, output_dim, lr=0.001, batch_size=51
         validation_loss.append(avg_val_loss)
         if avg_val_loss < best_loss:
             print("saving the best model so far")
-            state_dict_path = "/content/state_dict_best_model.pt"
+            state_dict_path = "/output/state_dict_best_model.pt"
             torch.save(model.state_dict(), state_dict_path)
+            best_state_dict = copy(model.state_dict())
             best_loss = avg_val_loss
         print(
             f"Epoch {epoch + 1}/{n_epochs}\t training_loss={avg_loss:.4f}"
             f"\t validation_loss={avg_val_loss: 4f} \t "
             f"time={elapsed_time:.2f}s"
         )
+    mlflow.log_param("best_loss", best_loss)
+    mlflow.log_param("last_epoch_loss", avg_val_loss)
+    mlflow.pytorch.log_model(best_state_dict, "extraction-model")
 
     return training_loss, validation_loss
 
