2aeb070e4ab76499357dc20b728822e17ef21346
sfekih <selim.fekih@epfl.ch>
2022-10-03 15:44:54 +0300

    update train script, still not working

--- scripts/training/selim/entry_extraction/scripts/train.py
+++ scripts/training/selim/entry_extraction/scripts/train.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append(".")
 
 import math
-import pickle
+import json
 import logging
 from typing import List
 from pathlib import Path
@@ -12,6 +12,7 @@ import os
 import argparse
 import torch
 import mlflow
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 import pytorch_lightning as pl
 from transformers import (
     TrainingArguments,
@@ -19,9 +20,6 @@ from transformers import (
 from transformers.hf_argparser import HfArgumentParser
 
 from model import TrainingExtractionModel, LoggedExtractionModel
-from data import (
-    LABEL_NAMES,
-)
 from inference import EntryExtractionWrapper
 
 logging.basicConfig(level=logging.INFO)
@@ -38,17 +36,14 @@ class Args:
     max_full_length: int
     max_length: int
     extra_context_length: int
+    learning_rate: float
     tracking_uri: str = MLFLOW_SERVER
     experiment_name: str = "entry_extraction"
     n_separate_layers: int = 0
-    separate_layer_groups: List[List[str]] = list
     token_loss_weight: float = 1.0
     sentence_edit_threshold: int = math.inf
     n_subsample: int = None
     compute_relevant_with_or: bool = False
-    loss_weights: List[float] = field(
-        default_factory=lambda: [1.0] + [0.0] * (len(LABEL_NAMES) - 1)
-    )
 
 
 def get_conda_env_specs():
@@ -70,7 +65,7 @@ def get_args():
     return args, training_args
 
 
-def get_separate_layer_groups(args):
+"""def get_separate_layer_groups(args):
     if args.separate_layer_groups is not None:
         separate_layer_groups = []
 
@@ -80,7 +75,7 @@ def get_separate_layer_groups(args):
             )
     else:
         separate_layer_groups = args.separate_layer_groups
-    return separate_layer_groups
+    return separate_layer_groups"""
 
 
 if __name__ == "__main__":
@@ -105,8 +100,13 @@ if __name__ == "__main__":
     args, training_args = get_args()
 
     # load data
-    with open(default_args.data_dir, "rb") as f:
-        train_dataset, test_dataset, val_dataset = pickle.load(f)
+    with open(default_args.data_dir, "r") as f:
+        full_data = json.load(f)
+
+    train_dataset = full_data["train"]
+    test_dataset = full_data["test"]
+    val_dataset = full_data["val"]
+    tagname_to_tagid = full_data["tagname_to_id"]
 
     # Set remote mlflow server
     mlflow.set_tracking_uri(args.tracking_uri)
@@ -138,17 +138,35 @@ if __name__ == "__main__":
 
         ##################### train model using pytorch lightning #####################
 
-        # to be changed later
-        early_stopping_callback = None
-        checkpoint_callback = None
+        MODEL_NAME = "transformer_model"
+        MODEL_DIR = default_args.model_dir
+        if not os.path.exists(MODEL_DIR):
+            os.makedirs(MODEL_DIR)
+
+        early_stopping_callback = EarlyStopping(
+            monitor="val_loss",
+            patience=1 + training_args.num_train_epochs // 3,
+            mode="min",
+        )
+
+        checkpoint_callback_params = {
+            "save_top_k": 1,
+            "verbose": True,
+            "monitor": "val_loss",
+            "mode": "min",
+        }
+
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=MODEL_DIR, filename=MODEL_NAME, **checkpoint_callback_params
+        )
 
         trainer = pl.Trainer(
             logger=None,
-            # callbacks=[early_stopping_callback, checkpoint_callback],
-            progress_bar_refresh_rate=20,
+            callbacks=[early_stopping_callback, checkpoint_callback],
+            # progress_bar_refresh_rate=20,
             profiler="simple",
             # log_gpu_memory=True,
-            weights_summary=None,
+            # weights_summary=None,
             gpus=gpu_nb,
             # precision=16,
             accumulate_grad_batches=1,
@@ -167,17 +185,18 @@ if __name__ == "__main__":
 
         training_model = TrainingExtractionModel(
             args.model_name_or_path,
-            num_labels=len(LABEL_NAMES),
-            token_loss_weight=args.token_loss_weight,
-            loss_weights=args.loss_weights,
+            num_labels=len(tagname_to_tagid),
             slice_length=args.max_length,
             extra_context_length=args.extra_context_length,
-            n_separate_layers=args.n_separate_layers,
-            separate_layer_groups=get_separate_layer_groups(args),
+            lr=...
         )
 
-        training_loader = training_model._get_loaders(train_dataset, train_params)
-        val_loader = training_model._get_loaders(val_dataset, **val_params)
+        training_loader = training_model._get_loaders(
+            train_dataset, train_params, training_mode=True
+        )
+        val_loader = training_model._get_loaders(
+            val_dataset, val_params, training_mode=True
+        )
 
         trainer.fit(training_model, training_loader, val_loader)
 
