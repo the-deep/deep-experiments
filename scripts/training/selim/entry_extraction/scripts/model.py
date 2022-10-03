c5a8e86eb4df51261418fdb9d0584064f2a9425c
sfekih <selim.fekih@epfl.ch>
2022-10-03 15:44:36 +0300

    working models

--- scripts/training/selim/entry_extraction/scripts/model.py
+++ scripts/training/selim/entry_extraction/scripts/model.py
@@ -1,94 +1,63 @@
 import pytorch_lightning as pl
 from torch import nn
 import torch
-from transformers import BertModel, BertConfig, AutoTokenizer
-from transformers.models.bert.modeling_bert import BertEncoder
-from utils import prepare_data_for_forward_pass
-from data import (
-    get_train_test_val_data,
-    LABEL_NAMES,
-    ExtractionDataset,
-)
+from torch.utils.data import DataLoader
+from transformers import AdamW, AutoTokenizer, AutoModel
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.nn.functional as F
+from data import ExtractionDataset
 
 
 class EntryExtractor(nn.Module):
     def __init__(
         self,
-        backbone,
-        num_labels,
-        n_separate_layers,
-        separate_layer_groups,
+        model_name_or_path: str,
+        num_labels: int,
+        slice_length: int,
+        n_freezed_layers: int = 1,
     ):
 
         super().__init__()
-        self.backbone = BertModel.from_pretrained(backbone)
+        self.common_backbone = AutoModel.from_pretrained(model_name_or_path)
+        self.common_backbone.encoder.layer = self.common_backbone.encoder.layer[:-1]
+        self.slice_length = slice_length
+        self.num_labels = num_labels
 
-        # split the backbone into separate layers
-        if n_separate_layers is not None and n_separate_layers > 0:
-            separate_layers_config = BertConfig.from_pretrained(backbone)
-            separate_layers_config.num_hidden_layers = n_separate_layers
+        # freeze embeddings
+        for param in self.common_backbone.embeddings.parameters():
+            param.requires_grad = False
+        # freeze n_freezed_layers first layers
+        if n_freezed_layers > 0:
+            for layer in self.common_backbone.encoder.layer[:n_freezed_layers]:
+                for param in layer.parameters():
+                    param.requires_grad = False
 
-            if separate_layer_groups is None:
-                separate_layer_groups = [[i] for i in range(num_labels)]
-
-            self.separate_layers = nn.ModuleList(
-                [
-                    BertEncoder(separate_layers_config)
-                    for _ in range(len(separate_layer_groups))
-                ]
-            )
-            self.separate_layer_groups = separate_layer_groups
-
-            for lr in self.separate_layers:
-                for i, layer in enumerate(lr.layer):
-                    layer.load_state_dict(
-                        self.backbone.encoder.layer[-n_separate_layers + i].state_dict()
-                    )
-
-            for _ in range(n_separate_layers):
-                del self.backbone.encoder.layer[-1]
-        else:
-            self.separate_layers = None
-
-        self.out_projs = nn.ModuleList(
-            [nn.Linear(self.backbone.config.hidden_size, 1) for _ in range(num_labels)]
+        self.separate_layers = torch.nn.ModuleList(
+            [
+                AutoModel.from_pretrained(model_name_or_path).encoder.layer[-1]
+                for _ in range(num_labels)
+            ]
         )
 
-    def forward(
-        self,
-        input_ids,  # 2d array of shape(n_steps, slice_length)
-        attention_mask,  # 2d array as above
-        token_labels,  # 3d array
-    ):
-        logits = torch.zeros_like(token_labels)
+    def forward(self, input_ids, attention_mask):
+        logits = []
 
-        hidden_state = self.backbone(
+        hidden_state = self.common_backbone(
             input_ids, attention_mask=attention_mask
         ).last_hidden_state
 
-        if self.separate_layers is None:
-            for i in range(self.num_labels):
-                logits[:, :, i] = self.out_projs[i](hidden_state)[..., 0]
-        else:
-            # the attention mask passed to `BertEncoder.forward` is an "extended" attention mask
-            # (not the same as passed to `BertModel`) so we have to manually create it here
-            # to call `BertEncoder.forward` directly.
-            extended_attention_mask: torch.Tensor = (
-                self.backbone.get_extended_attention_mask(
-                    attention_mask, hidden_state.size()[:-1], self.backbone.device
-                )
-            )
+        for separate_layer_idx in range(self.num_labels):
+            h = self.separate_layers[separate_layer_idx](hidden_state.clone())[0]
 
-            for separate_layer_idx, label_group in enumerate(
-                self.separate_layer_groups
-            ):
-                h = self.separate_layers[separate_layer_idx](
-                    hidden_state, attention_mask=extended_attention_mask
-                ).last_hidden_state
+            cls_output = h[..., 0]
+            mean_pooling = torch.mean(h, dim=-1)
 
-                for i in label_group:
-                    logits[:, :, i] = self.out_projs[i](h)[..., 0]
-        return logits
+            output = (cls_output + mean_pooling) / 2
+
+            logits.append(
+                output  
+            )  
+        return torch.stack(logits, dim=2)
 
 
 class TrainingExtractionModel(pl.LightningModule):
@@ -96,12 +65,11 @@ class TrainingExtractionModel(pl.LightningModule):
         self,
         backbone,
         num_labels,
-        token_loss_weight,
-        loss_weights,
         slice_length,
         extra_context_length,
-        n_separate_layers=None,
-        separate_layer_groups=None,
+        lr: float = 1e-4,
+        adam_epsilon: float = 1e-7,
+        weight_decay: float = 1e-2,
     ):
         """
         Args:
@@ -110,7 +78,6 @@ class TrainingExtractionModel(pl.LightningModule):
             num_labels: number of labels
             token_loss_weight: weight of the token-level loss e.g. 0.5 will
                 result in even weighting of token-level and sentence-level loss
-            loss_weights: contribution of the individual labels to the loss
             slice_length: length of the context that is fed into the model at
                 once
             extra_context_length: length of prefix that will be fed to the
@@ -122,117 +89,125 @@ class TrainingExtractionModel(pl.LightningModule):
         """
         super().__init__()
 
-        self.entry_extraction_model = EntryExtractor(
-            backbone,
-            num_labels,
-            n_separate_layers,
-            separate_layer_groups,
-        )
+        self.entry_extraction_model = EntryExtractor(backbone, num_labels, slice_length)
 
         self.tokenizer = AutoTokenizer.from_pretrained(backbone)
         self.pad_token_id = self.tokenizer.pad_token_id
 
         self.num_labels = num_labels
-        self.token_loss_weight = token_loss_weight
         self.slice_length = slice_length
         self.extra_context_length = extra_context_length
+        self.lr = lr
+        self.adam_epsilon = adam_epsilon
+        self.weight_decay = weight_decay
 
-        # initialize loss function and loss weights
-        self.loss_fct = nn.BCEWithLogitsLoss(reduction="none")
-        self.loss_weights = (
-            torch.tensor(loss_weights).unsqueeze(0)
-            if loss_weights is not None
-            else None
+    def _compute_loss(self, logits, groundtruth):
+
+        token_loss = F.binary_cross_entropy_with_logits(
+            logits, groundtruth.float(), reduction="mean"
         )
 
-    def _compute_loss(self, logits, batch):
-        loss_weights = (
-            self.loss_weights.to(logits.device)
-            if self.loss_weights is not None
-            else 1.0
-        )
-
-        token_labels_mask = batch["token_labels_mask"]
-        token_labels = batch["token_labels"]
-
-        if token_labels_mask is not None:
-            active_loss = token_labels_mask.view(-1) == 1
-        else:
-            active_loss = batch["attention_mask"].view(-1) == 1
-        active_logits = logits.view(-1, self.num_labels)
-        active_labels = token_labels.view(-1, self.num_labels)
-
-        token_loss = (
-            (self.loss_fct(active_logits, active_labels) * loss_weights)
-            .sum(-1)[active_loss]
-            .mean()
-        )
-        # TODO: Add sentence loss as well
         return token_loss
 
     def forward(
         self,
-        input_ids,  # 2d array of shape(n_steps, slice_length)
-        attention_mask,  # 2d array as above
-        token_labels,  # 3d array
+        input_ids,
+        attention_mask,
+        loss_mask,
     ):
-        output = self.entry_extraction_model(input_ids, attention_mask, token_labels)
+        output = self.entry_extraction_model(input_ids, attention_mask)
+        output = output[torch.where(loss_mask == 1)]
         return output
 
     def _operate_train_or_val_step(self, batch):
         """
         batch: {
-            "id": d["id"],
             "input_ids": d["input_ids"],
             "attention_mask": d["attention_mask"],
-            "offset_mapping": d["offset_mapping"],
             "token_labels": d["token_labels"],
-            "token_labels_mask": d["token_labels_mask"],
-            "sentence_labels": d["sentence_labels"],
-            "sentence_labels_mask": d["sentence_labels_mask"],
+            "loss_mask": d["loss_mask"]
         }
         """
-        preprocessed_batch = prepare_data_for_forward_pass(
-            batch,
-            slice_length=self.slice_length,
-            extra_context_length=self.extra_context_length,
-            pad_token_id=self.pad_token_id,
-            num_labels=self.num_labels,
-            training=True,
-        )
 
-        # LOSS calculation
         logits = self(
-            preprocessed_batch["input_ids"],
-            preprocessed_batch["attention_mask"],
-            preprocessed_batch["token_labels"],
+            batch["input_ids"],
+            batch["attention_mask"],
+            batch["loss_mask"],
         )
 
-        loss = self._compute_loss(logits, preprocessed_batch)
+        mask = batch["loss_mask"]
+        important_labels = batch["token_labels"]
+        important_labels = important_labels[torch.where(mask == 1)]
+
+        loss = self._compute_loss(logits, important_labels)
 
         return loss
 
     def training_step(self, batch, batch_idx):
 
-        loss = self._operate_train_or_val_step(batch)
-        return loss
+        train_loss = self._operate_train_or_val_step(batch)
+
+        self.log(
+            "train_loss",
+            train_loss.item(),
+            prog_bar=True,
+            on_step=False,
+            on_epoch=True,
+            logger=False,
+        )
+
+        return train_loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self._operate_train_or_val_step(batch)
-        return loss
+        val_loss = self._operate_train_or_val_step(batch)
 
-    def predict_step(self, batch, batch_idx, dataloader_idx=None):
-        output = self(batch)
-        return {"logits": output}
+        self.log(
+            "val_loss",
+            val_loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=False,
+        )
+
+        return {"val_loss": val_loss}
 
     def configure_optimizers(self, *args, **kwargs):
-        pass
+        "Prepare optimizer and schedule (linear warmup and decay)"
 
-    def _get_loaders(self, data, params):
+        optimizer = AdamW(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+            eps=self.adam_epsilon,
+        )
+
+        # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+        scheduler = ReduceLROnPlateau(
+            optimizer, "min", factor=0.5, patience=1, threshold=1e-3
+        )
+
+        scheduler = {
+            "scheduler": scheduler,
+            "interval": "epoch",
+            "frequency": 1,
+            "monitor": "val_loss",
+        }
+        return [optimizer], [scheduler]
+
+    def _get_loaders(self, data, params, training_mode: bool):
         """
         get the dataloader from raw data
         """
-        ...
+        set = ExtractionDataset(
+            dset=data,  # dict if training and raw text input text if test
+            training_mode=training_mode,
+            tokenizer=self.tokenizer,
+            max_input_len=self.slice_length,
+            extra_context_length=self.extra_context_length,
+        )
+        loader = DataLoader(set, **params, pin_memory=True)
+        return loader
 
     def hypertune_threshold(self, validation_loader):
         """
@@ -249,30 +224,42 @@ class LoggedExtractionModel(nn.Module):
         # get all values needed for inference into new class
         # new class used for logging
         self.trained_entry_extraction_model = trained_model.entry_extraction_model
+        self.tokenizer = trained_model.tokenizer
         self.slice_length = trained_model.slice_length
         self.extra_context_length = trained_model.extra_context_length
-        self.pad_token_id = trained_model.pad_token_id
         self.num_labels = trained_model.num_labels
 
         self.test_params = {"batch_size": 16, "shuffle": False, "num_workers": 0}
 
-    def forward(self, batch):
-        preprocessed_data = prepare_data_for_forward_pass(
-            batch,
-            slice_length=self.slice_length,
-            extra_context_length=self.extra_context_length,
-            pad_token_id=self.pad_token_id,
-            num_labels=self.num_labels,
-            training=False,
-        )
-        outputs = self(preprocessed_data)
-        return outputs
+    def forward(
+        self,
+        input_ids,
+        attention_mask,
+        loss_mask,
+    ):
+        output = self.entry_extraction_model(input_ids, attention_mask)
+        output = output[torch.where(loss_mask == 1)]
+        return output
 
-    def _get_loaders(self, data, params):
+    def _get_loaders(self, data, params, training_mode: bool):
         """
         get the dataloader from raw data
+
+        returned batch: {
+            "input_ids": d["input_ids"],
+            "attention_mask": d["attention_mask"],
+            "loss_mask": d["loss_mask"]
+        }
         """
-        ...
+        set = ExtractionDataset(
+            dset=data,  # dict if training and raw text input text if test
+            training_mode=training_mode,
+            tokenizer=self.tokenizer,
+            max_input_len=self.slice_length,
+            extra_context_length=self.extra_context_length,
+        )
+        loader = DataLoader(set, **params, pin_memory=True)
+        return loader
 
     def get_highlights(self, raw_input_text):
         """
