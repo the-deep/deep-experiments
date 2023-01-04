1ed8be4407ac8c88e91298507fd7b2021562b475
Nicolò Tamagnone <nico@Nicolos-MBP.lan>
2023-01-04 19:11:06 +0100

    timeout leads extraction batch

--- notebooks/models/selim/data_v0.8/scripts/extract-leads-text/pull_leads_batch.py
+++ notebooks/models/selim/data_v0.8/scripts/extract-leads-text/pull_leads_batch.py
@@ -1,7 +1,7 @@
 import io
 import json
 import boto3
-# import timeout_decorator
+import timeout_decorator
 import pandas as pd
 
 from deep_parser import TextFromFile, TextFromWeb
@@ -10,6 +10,7 @@ from deep_parser.helpers.errors import DocumentProcessingError
 BUCKET_NAME: str = "pulled-leads"
 DATAFRAME_BUCKET_PATH: str = "projects-leads-urls/projects_leads_urls.csv"
 S3_RESOURCE = boto3.resource("s3", "us-east-1")
+BUCKET = S3_RESOURCE.Bucket(BUCKET_NAME)
 
 def get_projects_leads_dataset():
 
@@ -26,6 +27,14 @@ def get_projects_leads_dataset():
 
     return leads_df
 
+def check_document(projects_id: int, lead_id: int):
+
+    check = list(map(lambda x: x.key, BUCKET.objects.filter(
+        Delimiter="/", 
+        Prefix=f"{projects_id}/{lead_id}.json"))
+        )
+
+    return True if check else False
 
 def save_object(text: dict, project_id: int, lead_id: int):
 
@@ -34,45 +43,73 @@ def save_object(text: dict, project_id: int, lead_id: int):
         Body=(bytes(json.dumps(text).encode('UTF-8')))
     )
 
+@timeout_decorator.timeout(5 * 60)
+def extract_pdf(url, project_id, lead_id):
+    try:
+        parser = TextFromFile(url=url, from_web=True)
+        text, _ = parser.extract_text_multi(output_format="list")
+        save_object(text=text, project_id=project_id, lead_id=lead_id)
+        print(f"project-id: {project_id}, lead-id: {lead_id}, type: pdf. correctly processed")
+    except TimeoutError:
+        raise TimeoutError
+    except (RuntimeError, DocumentProcessingError, Exception) as e:
+        raise e
+
+@timeout_decorator.timeout(20)         
+def extract_website(url, project_id, lead_id):
+    try:
+        parser = TextFromWeb(url=url)
+        text = parser.extract_text(output_format="list")
+        parser.close()
+        save_object(text=text, project_id=project_id, lead_id=lead_id)
+        print(f"project-id: {project_id}, lead-id: {lead_id}, type: website. correctly processed")
+    except TimeoutError:
+        raise TimeoutError
+    except (RuntimeError, DocumentProcessingError, Exception) as e:
+        raise e
+    
 
 #@timeout_decorator.timeout(5 * 60, use_signals=False)
 def pull_websites(leads_df: pd.DataFrame):
 
-    print("###################### Start puling websites data.")
+    print("###################### Start pulling websites data.")
     
     for (project_id, lead_id), group in list(leads_df.groupby(["project_id", "lead_id"])):
+        
         project_id, lead_id = str(int(project_id)), str(int(lead_id))
+
+        if check_document(projects_id=project_id, lead_id=lead_id):
+            print(f"project-id: {project_id}, lead-id: {lead_id}, already present")
+            continue
+
         urls = group["url"].unique()
-
         assert len(urls) == 1
-
         url = urls[0]
 
         if not isinstance(url, str):  # possibly np.nan
             continue
+        
 
         if url.endswith(".pdf"):
             print(f"{url} is a PDF!")
             try:
-                parser = TextFromFile(url=url, from_web=True)
-                text, _ = parser.extract_text(output_format="list")
-                save_object(text=text, project_id=project_id, lead_id=lead_id)
-                print(f"project-id: {project_id}, lead-id: {lead_id}, type: pdf. correctly processed")
+                extract_pdf(url, project_id, lead_id)
+            except TimeoutError:
+                print(f"Timeout error. project-id: {project_id}, lead-id: {lead_id}")
             except (RuntimeError, DocumentProcessingError, Exception) as e:
-                print(f"Error {e} on PDF url {url}")
+                print(f"Error {e}. project-id: {project_id}, lead-id: {lead_id}")
                 continue
+
         else:
             try:
-                parser = TextFromWeb(url=url)
-                text = parser.extract_text(output_format="list")
-                parser.close()
-                save_object(text=text, project_id=project_id, lead_id=lead_id)
-                print(f"project-id: {project_id}, lead-id: {lead_id}, type: website. correctly processed")
+                extract_website(url, project_id, lead_id)
+            except TimeoutError:
+                print(f"Timeout error. project-id: {project_id}, lead-id: {lead_id}")
             except (RuntimeError, DocumentProcessingError, Exception) as e:
-                print(f"Error {e} on standard url {url}")
+                print(f"Error {e}. project-id: {project_id}, lead-id: {lead_id}")
                 continue
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    leads_df = get_projects_leads_dataset()
-    pull_websites(leads_df=leads_df)
+leads_df = get_projects_leads_dataset()
+pull_websites(leads_df=leads_df)
